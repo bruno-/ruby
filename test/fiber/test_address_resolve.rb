195d4eb35ec1f75326e0aa3b9e4dc713c1f63543
Bruno Sutic <code@brunosutic.com>
2020-12-09 01:08:40 +0100

    Improve tests

--- test/fiber/test_address_resolve.rb
+++ test/fiber/test_address_resolve.rb
@@ -3,16 +3,24 @@ require 'test/unit'
 require_relative 'scheduler'
 
 class TestAddressResolve < Test::Unit::TestCase
-  def test_addrinfo_getaddrinfo
-    finished_order = []
+  class NoopScheduler
+    def address_resolve(*)
+    end
+  end
 
+  class StubScheduler
+    def address_resolve(hostname, timeout = nil)
+      ["1.2.3.4", "1234:1234:123:1:123:1234:1234:1234"]
+    end
+  end
+
+  def test_addrinfo_getaddrinfo_localhost_non_blocking
     Thread.new do
-      scheduler = Scheduler.new
+      scheduler = NoopScheduler.new # scheduler hook not invoked
       Fiber.set_scheduler scheduler
 
       Fiber.schedule do
         result = Addrinfo.getaddrinfo("localhost", 80, :AF_INET, :STREAM)
-        finished_order << 1
         assert_equal(1, result.count)
 
         ai = result.first
@@ -22,12 +30,92 @@ class TestAddressResolve < Test::Unit::TestCase
         assert_equal(Socket::AF_INET, ai.afamily)
         assert_equal(Socket::SOCK_STREAM, ai.socktype)
       end
+    end.join
+  end
+
+  def test_addrinfo_getaddrinfo_domain_blocking
+    Thread.new do
+      scheduler = StubScheduler.new
+      Fiber.set_scheduler scheduler
 
       Fiber.schedule do
-        finished_order << 2
+        result = Addrinfo.getaddrinfo("example.com", 80, :AF_INET, :STREAM)
+        assert_equal(1, result.count)
+
+        ai = result.first
+        assert_equal("1.2.3.4", ai.ip_address)
+        assert_equal(80, ai.ip_port)
+        assert_equal(Socket::AF_INET, ai.afamily)
+        assert_equal(Socket::SOCK_STREAM, ai.socktype)
       end
     end.join
+  end
 
-    assert_equal([2, 1], finished_order) # TODO
+  def test_addrinfo_getaddrinfo_numeric_non_blocking
+    Thread.new do
+      scheduler = NoopScheduler.new # scheduler hook not invoked
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        result = Addrinfo.getaddrinfo("4.3.2.1", 80, :AF_INET, :STREAM)
+        assert_equal(1, result.count)
+
+        ai = result.first
+        assert_equal("1.2.3.4", ai.ip_address)
+        assert_equal(80, ai.ip_port)
+        assert_equal(Socket::AF_INET, ai.afamily)
+        assert_equal(Socket::SOCK_STREAM, ai.socktype)
+      end
+    end.join
+  end
+
+  def test_addrinfo_getaddrinfo_any_non_blocking
+    Thread.new do
+      scheduler = NoopScheduler.new # scheduler hook not invoked
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        result = Addrinfo.getaddrinfo("<any>", 80, :AF_INET, :STREAM)
+        assert_equal(1, result.count)
+
+        ai = result.first
+        assert_equal("0.0.0.0", ai.ip_address)
+        assert_equal(80, ai.ip_port)
+        assert_equal(Socket::AF_INET, ai.afamily)
+        assert_equal(Socket::SOCK_STREAM, ai.socktype)
+      end
+    end.join
+  end
+
+  def test_addrinfo_getaddrinfo_non_existing_domain_blocking
+    Thread.new do
+      scheduler = NoopScheduler.new # invoked, returns nil
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        error_msg = "getaddrinfo: nodename nor servname provided, or not known"
+        assert_raise_with_message(SocketError, error_msg) {
+          Addrinfo.getaddrinfo("non-existing-domain.abc", nil)
+        }
+      end
+    end.join
+  end
+
+  def test_addrinfo_getaddrinfo_no_host_non_blocking
+    Thread.new do
+      scheduler = NoopScheduler.new # scheduler hook not invoked
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        result = Addrinfo.getaddrinfo(nil, 80, :AF_INET, :STREAM)
+        assert_equal(1, result.count)
+
+        ai = result.first
+        assert_equal("127.0.0.1", ai.ip_address)
+        assert_equal(80, ai.ip_port)
+        assert_equal(Socket::AF_INET, ai.afamily)
+        assert_equal(Socket::SOCK_STREAM, ai.socktype)
+      end
+    end.join
   end
 end
